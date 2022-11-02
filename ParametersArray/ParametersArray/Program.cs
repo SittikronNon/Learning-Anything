3cbdb6aed1d777b6444229f5e5ee5d6ab5f4c569
SittikronNon <sittikron.lamulsab@gmail.com>
2022-11-03 00:30:46 +0700

    Parameters

---
+++ ParametersArray/ParametersArray/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ParametersArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] happiness = {12, 23, 43, 23, 50};
+            GetAverage(happiness);
+
+            foreach (int x in happiness)
+            { 
+                Console.WriteLine(x);
+            }
+            Console.ReadLine();
+        }
+
+
+        static void GetAverage(int[] happyArray)
+        {
+            for(int i = 0; i < happyArray.Length; i++)
+            {
+                happyArray[i] += 2;
+            }
+            
+        }
+    }
+}
