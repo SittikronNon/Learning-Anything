ade573b23c184a5af0e9348de354cc8173f57f04
SittikronNon <sittikron.lamulsab@gmail.com>
2022-11-07 23:20:15 +0700

    ArrayList and List

---
+++ List/List/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+
+namespace List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList myArrayList = new ArrayList();
+            ArrayList myArrayList2 = new ArrayList();
+        }
+    }
+}
