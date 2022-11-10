e8c2f326115408cca6639e92fa7f2e2c4a63f974
SittikronNon <sittikron.lamulsab@gmail.com>
2022-11-10 23:31:59 +0700

    Update Program.cs

--- Dictionaries/Dictionaries/Program.cs
+++ Dictionaries/Dictionaries/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Dictionaries
 {
     internal class Program
     {
+        //key = value
+        //Auto(german) = car
+
         static void Main(string[] args)
         {
+            Dictionary<int, string> myDictionary = new Dictionary<int, string>()
+            {
+                {1, "one" },
+                {2, "two" },
+                {3, "three" }
+            };
+
             Console.WriteLine("Hello World!");
+        }
+    }
+
+    class Employee 
+    {
+        public string Role { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public float Rate { get; set; }
+
+        public float Salary
+        {
+
         }
     }
 }
