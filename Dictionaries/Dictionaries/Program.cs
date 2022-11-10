d28c9c0923d1d08a1ae547fd687ae26a82d03a20
SittikronNon <sittikron.lamulsab@gmail.com>
2022-11-11 00:33:50 +0700

    Dictionaries Updated

--- Dictionaries/Dictionaries/Program.cs
+++ Dictionaries/Dictionaries/Program.cs
@@ -8,8 +8,20 @@ namespace Dictionaries
         //key = value
         //Auto(german) = car
 
+
+      
         static void Main(string[] args)
         {
+            Employee[] employees =
+            {
+                new Employee("CEO","Gwyn",95,200),
+                new Employee("HR","Lora",35,25),
+                new Employee("Manager","Joe",32,21),
+                new Employee("SEA","Non",45,200),
+                new Employee("SEO","Nick",100,98),
+                new Employee("SE","Mind",45,200),
+            };
+
             Dictionary<int, string> myDictionary = new Dictionary<int, string>()
             {
                 {1, "one" },
@@ -17,11 +29,45 @@ namespace Dictionaries
                 {3, "three" }
             };
 
-            Console.WriteLine("Hello World!");
+            Dictionary<string, Employee> employeesDirectory = new Dictionary<string, Employee>();
+
+            foreach (Employee emp in employees)
+            {
+                employeesDirectory.Add(emp.Role, emp);
+            }
+
+            string key = "SEAa";
+            if (employeesDirectory.ContainsKey(key))
+            {
+                Employee empl = employeesDirectory["SEA"];
+
+                Console.WriteLine("Employee Name: {0}, Role: {1}, Salary: {2}", empl.Name, empl.Role, empl.Salary);
+            }
+            else
+            {
+                Console.WriteLine("No employee found with this key '{0}'", key);
+            }
+
+            Employee result = null;
+            if (employeesDirectory.TryGetValue("CEO", out result))
+            {
+                Console.WriteLine("Value Retrieved!.");
+
+                Console.WriteLine("Employee Name: {0}", result.Name);
+                Console.WriteLine("Employee Name: {0}", result.Role);
+                Console.WriteLine("Employee Name: {0}", result.Age);
+                Console.WriteLine("Employee Name: {0}", result.Salary);
+
+            }
+            else
+            {
+                Console.WriteLine("The key does not exist");
+            }
+
         }
     }
 
-    class Employee 
+    class Employee
     {
         public string Role { get; set; }
         public string Name { get; set; }
@@ -30,7 +76,18 @@ namespace Dictionaries
 
         public float Salary
         {
+            get
+            {
+                return Rate * 8 * 5 * 4 * 12;
+            }
+        }
 
+        public Employee(string role, string name, int age, float rate)
+        {
+            this.Role = role;
+            this.Name = name;
+            this.Age = age;
+            this.Rate = rate;
         }
     }
 }
