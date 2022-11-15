5953645297f1853d5a4172ce0265fcd4711b9b14
SittikronNon <sittikron.lamulsab@gmail.com>
2022-11-16 00:27:47 +0700

    Absctact1

---
+++ c#/AbstractClassesC/AbstractClassesC/Cube.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClassesC
+{
+    internal class Cube : Shape
+    {
+        public int Length { get; set; }
+
+        public Cube(int length)
+        {
+            Name = "Cube";
+            Length = length;
+        }
+
+        public override double Volume()
+        {
+            return Math.Pow(Length, 3);
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"\nThe Cube has a length of {Length}");
+        }
+    }
+}
---
+++ c#/AbstractClassesC/AbstractClassesC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace AbstractClassesC
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape[] shapes = { new Sphere(4), new Cube(3) };
+
+            foreach(Shape shape in shapes)
+            {
+                shape.GetInfo();
+                Console.WriteLine("{0} has a volume of {1}", shape.Name, shape.Volume());
+
+                Cube iceCube = shape as Cube;
+                if(iceCube == null)
+                {
+                    Console.WriteLine("This shape is no cube");
+                }
+
+                if(shape is Cube)
+                {
+                    Console.WriteLine("This shape a cube");
+                }
+            }
+
+        }
+    }
+}
