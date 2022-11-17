defc8248f2e79aedf4553b781fe1bac3231541b9
SittikronNon <sittikron.lamulsab@gmail.com>
2022-11-17 22:45:19 +0700

    TextFile2

---
+++ c#/Assignment2_file/Assignment2_file/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Assignment2_file
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- c#/FileIOc/FileIOc/Program.cs
+++ c#/FileIOc/FileIOc/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace FileIOc
 {
@@ -6,8 +8,43 @@ namespace FileIOc
     {
         static void Main(string[] args)
         {
+
+            // Method 1
+            string[] lines = { "First 252", "Second 242", "Third 240" };
+
+            File.WriteAllLines(@"C:\Users\User\Desktop\c#\c#/highscore.txt", lines);
+
+            /*
+            // Method 2
+            Console.WriteLine("Please give the file a name");
+            string fileName = Console.ReadLine();
+            Console.WriteLine("Please enter the text for the file");
+            string input = Console.ReadLine();
+            File.WriteAllText(@"C:\Users\User\Desktop\c#\c#/" + fileName + ".txt", input);
+            */
+
+            // Method 3
+            using(StreamWriter file = new StreamWriter(@"C:\Users\User\Desktop\c#\c#/myText.txt"))
+            {
+                foreach(string line in lines)
+                {
+                    if(line.Contains("2"))
+                    {
+                        file.WriteLine(line);
+                    }    
+                }
+            }    
+
+            using(StreamWriter file = new StreamWriter(@"C:\Users\User\Desktop\c#\c#/myText2.txt", true))
+            {
+                file.WriteLine("Additional line");
+            }
+
+
+
+
             // reading Text
-            string text = System.IO.File.ReadAllText(@"C:\Users\User\Desktop\c#\c#/textFile.txt");
+           /*string text = System.IO.File.ReadAllText();
 
             Console.WriteLine("Textfile contains following text: {0}", text);
 
@@ -19,7 +56,7 @@ namespace FileIOc
                 Console.WriteLine("\t" + line);
             }
 
-            Console.ReadKey();
+            Console.ReadKey();*/
         }
     }
 }
