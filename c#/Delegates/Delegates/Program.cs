09a488cafdc756ed4fd9a50c957912dca1c11e1a
SittikronNon <sittikron.lamulsab@gmail.com>
2022-11-22 22:20:20 +0700

    delegates

---
+++ c#/Delegates/Delegates/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Delegates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Amatoli" };
+            
+        }
+    }
+}
