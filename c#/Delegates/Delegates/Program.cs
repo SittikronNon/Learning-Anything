6b5e135a46c4d433cb25f49a5d311d0694d84d28
SittikronNon <sittikron.lamulsab@gmail.com>
2022-11-24 00:47:34 +0700

    Anonymous Methods

--- c#/Delegates/Delegates/Program.cs
+++ c#/Delegates/Delegates/Program.cs
@@ -8,7 +8,28 @@ namespace Delegates
         static void Main(string[] args)
         {
             List<string> names = new List<string>() { "Aiden", "Sif", "Walter", "Amatoli" };
+
+            Console.WriteLine("-----before-----");
+            foreach (string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
+        
+            names.RemoveAll(Filter);
+
+            Console.WriteLine("-----after-----");
+            foreach(string name in names)
+            {
+                Console.WriteLine(name);
+            }
+
             
+        }
+
+        static bool Filter(string s)
+        {
+            return s.Contains("i");
         }
     }
 }
---
+++ c#/DelegatesDemo/DelegatesDemo/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+namespace DelegatesDemo
+{
+    internal class Program
+    {
+        public delegate bool FilterDelegate(Person p);
+
+        static void Main(string[] args)
+        {
+            Person p1 = new Person() { Name = "Aiden", Age = 41 };
+            Person p2 = new Person() { Name = "Sif", Age = 69 };
+            Person p3 = new Person() { Name = "Walter", Age = 12 };
+            Person p4 = new Person() { Name = "Anatoli", Age = 25 };
+
+            List<Person> people = new List<Person>() { p1, p2, p3, p4 };
+
+            DisplayPeople("Kids", people, IsMinor);
+            DisplayPeople("Adult", people, IsAdult);
+            DisplayPeople("Senior", people, IsSenior);
+
+            FilterDelegate filter = delegate (Person p)
+            {
+                return p.Age >= 20 && p.Age <= 30;
+            };
+
+            DisplayPeople("Between 20 and 30", people, filter);
+
+            DisplayPeople("ALL", people, delegate (Person p) { return true; });
+
+        }
+
+        static void DisplayPeople(string title, List<Person> people, FilterDelegate filter)
+        {
+            Console.WriteLine(title);
+
+            foreach(Person p in people)
+            {
+                if (filter(p))
+                {
+                    Console.WriteLine("{0}, {1} years old", p.Name, p.Age);
+                }
+            }
+        }
+
+        static bool IsMinor(Person p)
+        {
+            return p.Age < 18;
+        }
+
+        static bool IsAdult(Person p)
+        {
+            return p.Age >= 18;
+        }
+
+        static bool IsSenior(Person p)
+        {
+            return p.Age >= 65;
+        }
+    }
+}
