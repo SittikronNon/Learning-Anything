f875029f7de64efe92e0b947dd2e202ed74f3d3c
SittikronNon <sittikron.lamulsab@gmail.com>
2022-12-15 02:08:20 +0700

    updatedLinq

--- c#/LinqAndSQL/LinqAndSQL/MainWindow.xaml.cs
+++ c#/LinqAndSQL/LinqAndSQL/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,108 @@ namespace LinqAndSQL
     /// </summary>
     public partial class MainWindow : Window
     {
+        LinqToSqlDataClassesDataContext dataContext;
         public MainWindow()
         {
             InitializeComponent();
+            
+            string connectionString = ConfigurationManager.ConnectionStrings["LinqAndSQL.Properties.Settings.SittikronDBConnectionString"].ConnectionString;
+            dataContext = new LinqToSqlDataClassesDataContext(connectionString);
+
+            //InsertUniversity();
+            //InsertStudent();
+            //InsertLectures();
+            //InsertStudentLectureAssociations();
+            //GetUniversityTony();
+
+            GetLecturesFromTony();
+        }
+
+        public void InsertUniversity()
+        {
+            dataContext.ExecuteCommand("delete from University");
+
+            
+            University yale = new University();
+            University newcastle = new University();
+            yale.Name = "Yale";s
+            newcastle.Name = "Newcastle";
+            dataContext.Universities.InsertOnSubmit(yale);
+            dataContext.Universities.InsertOnSubmit(newcastle);
+
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Universities;
+        }
+
+        public void InsertStudent()
+        {
+            University yale = dataContext.Universities.First(un => un.Name.Equals("Yale"));
+            University newcastle = dataContext.Universities.First(un => un.Name.Equals("Newcastle"));
+
+            List<Student> students = new List<Student>();
+
+            students.Add(new Student { Name = "Nick", Gender = "Male", UniversityId = yale.Id });
+            students.Add(new Student { Name = "Tony", Gender = "Male", UniversityId = newcastle.Id });
+            students.Add(new Student { Name = "Minnie", Gender = "Female", University = yale});
+            students.Add(new Student { Name = "Mind", Gender = "Female", University = newcastle });
+            dataContext.Students.InsertAllOnSubmit(students);
+            dataContext.SubmitChanges();
+
+            MainDataGrid.ItemsSource = dataContext.Students;
+        }
+
+        public void InsertLectures()
+        {
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "Math" });
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "History" });
+            dataContext.Lectures.InsertOnSubmit(new Lecture { Name = "English" });
+
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.Lectures;
+
+        }
+
+        public void InsertStudentLectureAssociations()
+        {
+            Student Carla = dataContext.Students.First(st => st.Name.Equals("Nick"));
+            Student Tony = dataContext.Students.First(st => st.Name.Equals("Tony"));
+            Student Minnie = dataContext.Students.First(st => st.Name.Equals("Minnie"));
+            Student Mind = dataContext.Students.First(st => st.Name.Equals("Mind"));
+
+            Lecture Math = dataContext.Lectures.First(lc => lc.Name.Equals("Math"));
+            Lecture History = dataContext.Lectures.First(lc => lc.Name.Equals("History"));
+            Lecture English = dataContext.Lectures.First(lc => lc.Name.Equals("English"));
+
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Carla, Lecture = Math });
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Tony, Lecture = Math });
+
+            StudentLecture slMind = new StudentLecture();
+            slMind.Student = Mind;
+            slMind.LectureId = History.Id;
+            dataContext.StudentLectures.InsertOnSubmit(slMind);
+            dataContext.StudentLectures.InsertOnSubmit(new StudentLecture { Student = Minnie, Lecture = English });
+            dataContext.SubmitChanges();
+            MainDataGrid.ItemsSource = dataContext.StudentLectures;
+        }
+
+        public void GetUniversityTony()
+        {
+            Student Tony = dataContext.Students.First(st => st.Name.Equals("Tony"));
+
+            University TonyUniversity = Tony.University;
+
+            List<University> universities = new List<University>();
+            universities.Add(TonyUniversity);
+
+            MainDataGrid.ItemsSource = universities;
+        }
+
+        public void GetLecturesFromTony()
+        {
+            Student Tony = dataContext.Students.First(st => st.Name.Equals("Tony"));
+            var tonyLectures = from sl in Tony.StudentLectures select sl.Lecture;
+            MainDataGrid.ItemsSource = tonyLectures;
         }
     }
 }
--- c#/LinqAndSQL/LinqAndSQL/Properties/Settings.Designer.cs
+++ c#/LinqAndSQL/LinqAndSQL/Properties/Settings.Designer.cs
@@ -8,22 +8,40 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace LinqAndSQL.Properties
-{
-
-
+namespace LinqAndSQL.Properties {
+    
+    
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
-    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase
-    {
-
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.4.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
-
-        public static Settings Default
-        {
-            get
-            {
+        
+        public static Settings Default {
+            get {
                 return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-92AGDS1\\SITTIKRONSQL;Initial Catalog=SittikronDB;Persist Secu" +
+            "rity Info=True;User ID=sa;Password=nona7x_147")]
+        public string SittikronDBConnectionString {
+            get {
+                return ((string)(this["SittikronDBConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=DESKTOP-92AGDS1\\SITTIKRONSQL;Initial Catalog=SittikronDB;Persist Secu" +
+            "rity Info=True;User ID=sa")]
+        public string SittikronDBConnectionString1 {
+            get {
+                return ((string)(this["SittikronDBConnectionString1"]));
             }
         }
     }
